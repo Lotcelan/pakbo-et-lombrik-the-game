e1a264161521d273de97fdda4817d648e512b7d0
Lancelot VANRULLEN <lancelot.vanrullen@telecomnancy.net>
2024-03-15 08:30:08 +0100

    [+] Hotfix nul

--- src/engine/weapon.c
+++ src/engine/weapon.c
@@ -2,9 +2,13 @@
 #include"include/weapon.h"
 
 int delete_weapon(Weapon_t *weapon){
-    if(weapon->current_pv == 0){
+	/* 
+if(weapon->current_pv == 0){
         return 1;
     } else {
         return 0;
         }
+	*/
+
+	return 0;
 }
