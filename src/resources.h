e0b90cfe70d264c5a1c6f448c596db418173fb8c
SachaBnk <82036135+SachaBnk@users.noreply.github.com>
2024-04-20 19:18:11 +0200

    animation de mort de lombric (et création de la scène game over)

--- src/resources.h
+++ src/resources.h
@@ -5,36 +5,42 @@
 #include "engine/include/assets.h"
 #include <string.h>
 
-HashTable* init_resources();
-extern unsigned char src_assets_birck_background_png[];
-extern int src_assets_birck_background_png_len;
+HashTable* init_resources(void);
+extern unsigned char src_assets_projectile_laser_png[];
+extern int src_assets_projectile_laser_png_len;
 
 extern unsigned char src_assets_psych_duck_png[];
 extern int src_assets_psych_duck_png_len;
 
-extern unsigned char src_assets_bouth_png[];
-extern int src_assets_bouth_png_len;
+extern unsigned char src_assets_tentacula_png[];
+extern int src_assets_tentacula_png_len;
 
-extern unsigned char src_assets_duck_blue_png[];
-extern int src_assets_duck_blue_png_len;
-
-extern unsigned char src_assets_projectile_arrow_png[];
-extern int src_assets_projectile_arrow_png_len;
+extern unsigned char src_assets_lombric_png[];
+extern int src_assets_lombric_png_len;
 
 extern unsigned char src_assets_resized_background_png[];
 extern int src_assets_resized_background_png_len;
 
-extern unsigned char src_assets_dirt_png[];
-extern int src_assets_dirt_png_len;
+extern unsigned char src_assets_sol_png[];
+extern int src_assets_sol_png_len;
 
-extern unsigned char src_assets_black_png[];
-extern int src_assets_black_png_len;
+extern unsigned char src_assets_server_png[];
+extern int src_assets_server_png_len;
 
-extern unsigned char src_assets_duck_purple_png[];
-extern int src_assets_duck_purple_png_len;
+extern unsigned char src_assets_continue_png[];
+extern int src_assets_continue_png_len;
 
-extern unsigned char src_assets_random_lombric_walk_png[];
-extern int src_assets_random_lombric_walk_png_len;
+extern unsigned char src_assets_plateforme_surelevee_png[];
+extern int src_assets_plateforme_surelevee_png_len;
+
+extern unsigned char src_assets_Dirt_png[];
+extern int src_assets_Dirt_png_len;
+
+extern unsigned char src_assets_duck_orange_png[];
+extern int src_assets_duck_orange_png_len;
+
+extern unsigned char src_assets_projectile_arrow_png[];
+extern int src_assets_projectile_arrow_png_len;
 
 extern unsigned char src_assets_lombric_walk_png[];
 extern int src_assets_lombric_walk_png_len;
@@ -42,70 +48,70 @@ extern int src_assets_lombric_walk_png_len;
 extern unsigned char src_assets_sol_eclaire_png[];
 extern int src_assets_sol_eclaire_png_len;
 
-extern unsigned char src_assets_blurred_brick_background_with_floor_png[];
-extern int src_assets_blurred_brick_background_with_floor_png_len;
-
-extern unsigned char src_assets_hp_full_png[];
-extern int src_assets_hp_full_png_len;
-
-extern unsigned char src_assets_duck_orange_png[];
-extern int src_assets_duck_orange_png_len;
-
-extern unsigned char src_assets_server_png[];
-extern int src_assets_server_png_len;
-
-extern unsigned char src_assets_random_block_png[];
-extern int src_assets_random_block_png_len;
-
-extern unsigned char src_assets_ecocup_png[];
-extern int src_assets_ecocup_png_len;
-
-extern unsigned char src_assets_duck_red_png[];
-extern int src_assets_duck_red_png_len;
-
-extern unsigned char src_assets_lombric_png[];
-extern int src_assets_lombric_png_len;
-
-extern unsigned char src_assets_door_png[];
-extern int src_assets_door_png_len;
-
-extern unsigned char src_assets_wall_jpg[];
-extern int src_assets_wall_jpg_len;
+extern unsigned char src_assets_duck_purple_png[];
+extern int src_assets_duck_purple_png_len;
 
 extern unsigned char src_assets_blurred_brick_background_with_floor_copie_png[];
 extern int src_assets_blurred_brick_background_with_floor_copie_png_len;
 
+extern unsigned char src_assets_blue_canard_boss_png[];
+extern int src_assets_blue_canard_boss_png_len;
+
 extern unsigned char src_assets_toolbox_png[];
 extern int src_assets_toolbox_png_len;
 
-extern unsigned char src_assets_blue_canard_boss_png[];
-extern int src_assets_blue_canard_boss_png_len;
+extern unsigned char src_assets_duck_red_png[];
+extern int src_assets_duck_red_png_len;
+
+extern unsigned char src_assets_black_png[];
+extern int src_assets_black_png_len;
+
+extern unsigned char src_assets_background_png[];
+extern int src_assets_background_png_len;
+
+extern unsigned char src_assets_random_lombric_walk_png[];
+extern int src_assets_random_lombric_walk_png_len;
+
+extern unsigned char src_assets_wormcan_png[];
+extern int src_assets_wormcan_png_len;
+
+extern unsigned char src_assets_birck_background_png[];
+extern int src_assets_birck_background_png_len;
+
+extern unsigned char src_assets_bouth_png[];
+extern int src_assets_bouth_png_len;
+
+extern unsigned char src_assets_door_png[];
+extern int src_assets_door_png_len;
+
+extern unsigned char src_assets_duck_blue_png[];
+extern int src_assets_duck_blue_png_len;
+
+extern unsigned char src_assets_hp_full_png[];
+extern int src_assets_hp_full_png_len;
+
+extern unsigned char src_assets_wall_jpg[];
+extern int src_assets_wall_jpg_len;
+
+extern unsigned char src_assets_ecocup_png[];
+extern int src_assets_ecocup_png_len;
+
+extern unsigned char src_assets_random_block_png[];
+extern int src_assets_random_block_png_len;
+
+extern unsigned char src_assets_block_circles_png[];
+extern int src_assets_block_circles_png_len;
+
+extern unsigned char src_assets_blurred_brick_background_with_floor_png[];
+extern int src_assets_blurred_brick_background_with_floor_png_len;
+
+extern unsigned char src_assets_blurred_brick_background_with_floor_copie_3_png[];
+extern int src_assets_blurred_brick_background_with_floor_copie_3_png_len;
 
 extern unsigned char src_assets_hp_dead_png[];
 extern int src_assets_hp_dead_png_len;
 
 extern unsigned char src_assets_hp_half_png[];
 extern int src_assets_hp_half_png_len;
-
-extern unsigned char src_assets_projectile_laser_png[];
-extern int src_assets_projectile_laser_png_len;
-
-extern unsigned char src_assets_block_circles_png[];
-extern int src_assets_block_circles_png_len;
-
-extern unsigned char src_assets_sol_png[];
-extern int src_assets_sol_png_len;
-
-extern unsigned char src_assets_plateforme_surelevee_png[];
-extern int src_assets_plateforme_surelevee_png_len;
-
-extern unsigned char src_assets_blurred_brick_background_with_floor_copie_3_png[];
-extern int src_assets_blurred_brick_background_with_floor_copie_3_png_len;
-
-extern unsigned char src_assets_tentacula_png[];
-extern int src_assets_tentacula_png_len;
-
-extern unsigned char src_assets_background_png[];
-extern int src_assets_background_png_len;
 
 #endif // RESOURCES_H
